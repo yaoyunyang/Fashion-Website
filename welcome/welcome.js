e35841fea812c08db6bbeded0b09e56fafd21aed
Nineteen_N_E <m18827608163@163.com>
2020-05-16 01:57:05 +0800

    add avatar in welcome page

--- welcome/welcome.js
+++ welcome/welcome.js
@@ -5,8 +5,17 @@ function show_input() {
 }
 
 function confirm() {
+    name_list = ['zl', 'hyp', 'wzx', 'lzy', 'lwk', 'yxy', 'wl', 'cj', 'cyx', 'hh', 'qdy', 'hjc', 'zzq', 'hz', 'gws', 'zzr', 'mxl', 'nrx', 'gwj', 'kyl', 'ljy', 'hzb', 'wzg', 'yh', 'zf', 'zgb', 'ly', 'zt', 'wyz', 'zb'];
     input_value = document.getElementById("input_rectangle").value;
-    alert(input_value);
+    if (name_list.indexOf(input_value) == -1) {
+        alert('you are not graduate for fashion crew');
+    } else {
+        document.getElementById("imagePreview").style.backgroundImage = "url(http://39.102.67.244/wp-content/uploads/2020/05/avatar/" + input_value.toLowerCase() + ".jpg)";
+        document.querySelector(".welcome").style.display = "none";
+        document.querySelector(".avatar").style.opacity = 1;
+        document.getElementById("graduate_name").innerHTML = input_value.toUpperCase();
+    }
+    // alert(document.getElementById("imagePreview").style.backgroundImage)
 }
 
 function get_value() {
