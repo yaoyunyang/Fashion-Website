048750b243a7a3d138a73ec359ae6e0762ebceb2
Nineteen_N_E <m18827608163@163.com>
2020-05-17 00:52:06 +0800

    add the eror login prompt

--- welcome/welcome.js
+++ welcome/welcome.js
@@ -1,26 +1,48 @@
-function show_input() {
-    document.getElementById("input_rectangle").style.display = "inline-block";
-    document.getElementById("enter").style.display = "none";
-    document.querySelector(".confirm-btn").style.display = "inline-block";
-}
+$(function() {
+    var flag = 0;
+    $('#enter').on('click', function() {
+        document.getElementById("input_rectangle").style.display = "inline-block";
+        document.getElementById("enter").style.display = "none";
+        document.querySelector(".confirm-btn").style.display = "inline-block";
+    });
 
-function confirm() {
-    name_list = ['zl', 'hyp', 'wzx', 'lzy', 'lwk', 'yxy', 'wl', 'cj', 'cyx', 'hh', 'qdy', 'hjc', 'zzq', 'hz', 'gws', 'zzr', 'mxl', 'nrx', 'gwj', 'kyl', 'ljy', 'hzb', 'wzg', 'yh', 'zf', 'zgb', 'ly', 'zt', 'wyz', 'zb'];
-    input_value = document.getElementById("input_rectangle").value;
-    if (name_list.indexOf(input_value) == -1) {
-        alert('you are not graduate for fashion crew');
-    } else {
-        document.getElementById("imagePreview").style.backgroundImage = "url(http://39.102.67.244/wp-content/uploads/2020/05/avatar/" + input_value.toLowerCase() + ".jpg)";
-        document.querySelector(".welcome").style.display = "none";
-        document.querySelector(".avatar").style.opacity = 1;
-        document.getElementById("graduate_name").innerHTML = input_value.toUpperCase();
-    }
-    // alert(document.getElementById("imagePreview").style.backgroundImage)
-}
+    $('#confirm_btn').on('click', function() {
+        var name_list = ['zl', 'hyp', 'wzx', 'lzy', 'lwk', 'yxy', 'wl', 'cj', 'cyx', 'hh', 'qdy', 'hjc', 'zzq', 'hz', 'gws', 'zzr', 'mxl', 'nrx', 'gwj', 'kyl', 'ljy', 'hzb', 'wzg', 'yh', 'zf', 'zgb', 'ly', 'zt', 'wyz', 'zb'];
+        var input_value = document.getElementById("input_rectangle").value;
+        if (name_list.indexOf(input_value) == -1) {
+            // document.querySelector(".layer_mask").style.display = "block";
+            if (input_value == 'vistor') {
+                document.querySelector(".welcome").style.display = "none";
+                document.querySelector(".avatar").style.opacity = 1;
+            } else {
+                document.querySelector(".layer_mask").style.width = "100%";
+                document.querySelector(".layer_mask").style.transition = "width 1s ease";
+                document.querySelector(".welcome").style.opacity = 0;
+                flag = 1;
+            }
+        } else {
+            document.getElementById("imagePreview").style.backgroundImage = "url(http://39.102.67.244/wp-content/uploads/2020/05/avatar/" + input_value.toLowerCase() + ".jpg)";
+            document.querySelector(".welcome").style.display = "none";
+            document.querySelector(".avatar").style.opacity = 1;
+            document.getElementById("graduate_name").innerHTML = input_value.toUpperCase();
+        }
+    });
 
-function get_value() {
-    input_value = document.getElementById("input_rectangle").value;
-    if (input_value == "") {
-        document.getElementById("input_rectangle").placeholder = "name";
-    }
-}
+    $('#input_rectangle').blur(function() {
+        var input_value = document.getElementById("input_rectangle").value;
+        console.log(input_value);
+        if (input_value == "") {
+            document.getElementById("input_rectangle").placeholder = "name";
+        }
+    });
+
+    $('.layer_mask').on('click', function() {
+        if (flag == 1) {
+            console.log('hello world');
+            document.querySelector(".layer_mask").style.width = 0;
+            document.querySelector(".layer_mask").style.transition = "none";
+            document.querySelector(".welcome").style.opacity = 1;
+            flag = 0;
+        }
+    })
+})
